0f9b9625e96ed16a05971442e58316ea44114c24
HJELLO <HJELLO@DESKTOP-H572DK8>
2025-11-06 17:58:42 +0530

    Email, added new POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectutility/EmailPage.java
@@ -0,0 +1,24 @@
+package com.comcast.crm.objectutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmailPage {
+	
+	WebDriver driver;
+	public EmailPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(linkText = "My Mails")
+	private WebElement myMailLink;
+	
+	public WebElement getMyMailLink() {
+		return myMailLink;
+	}
+
+}
